db358e89d46dc56571df9714338d3f61ac628f68
Alisson Divino <124846427+Alisso19n@users.noreply.github.com>
2025-01-27 20:23:20 -0300

    Teste

--- biblioteca/src/br/edu/ifba/biblioteca/Main.java
+++ biblioteca/src/br/edu/ifba/biblioteca/Main.java
@@ -5,6 +5,7 @@ import br.edu.ifba.biblioteca.view.BibliotecaUi;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Teste");
         BibliotecaUi bibliotecaUI = new BibliotecaUi();
         bibliotecaUI.iniciar();
     }
